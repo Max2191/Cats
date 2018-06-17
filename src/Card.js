600a34fb80deedbddae6cb2fdb2ea8e09241a416
Max <maxtucker2191@gmail.com>
2018-06-17 22:07:26 +0100

    Changing from robots to cats theme!

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Card = ({name, email, id}) => {
+	
+	return (
+		<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+		
+			<img alt ="Cat is busy" src={`https://robohash.org/set_set4/${id}`} />
+			<div> 
+			<h2>{name}</h2>
+			<p>{email}</p>
+
+			</div>
+
+		</div>
+
+		)
+}
+
+
+
+export default Card; 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
+import { cats } from "./Cats";
 
-ReactDOM.render(<Hello greeting={'Hello React Ninja'}/>, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+	<Card id={cats[0].id} name={cats[0].name} email={cats[0].email} /> 
+	<Card id={cats[1].id} name={cats[1].name} email={cats[1].email}/>
+	<Card id={cats[2].id} name={cats[2].name} email={cats[2].email}/>
+
+
+
+	</div>, document.getElementById('root'));
 registerServiceWorker();
