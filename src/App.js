5896cf7fb0a5b99f88b8647ae052823b4b27c052
Max <maxtucker2191@gmail.com>
2018-06-20 19:51:29 +0100

    Added State mechanics and searchfield functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import CardList from "./CardList";
 import { cats } from "./Cats";
 import SearchBox from "./SearchBox";
 
-const App = () => {
 
-return (
-	<div className="tc">
-<h1>Cats!</h1>
-	<SearchBox />
-    <CardList cats={cats} />
-    </div>
+
+
+
+
+class App extends Component {
+	constructor() {
+		super()
+	this.state = {
+	cats: cats,
+	searchfield: "" 
+ 	}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value})
+		
+	
+	}
+
+	render () {
+		const filteredCats = this.state.cats.filter(cats => {
+		return cats.name.toLowerCase().includes(this.state.searchfield.toLowerCase());	
+		})
+     return (
+		<div className="tc">
+		    <h1>Cats!</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+		    <CardList cats={filteredCats} />
+	    </div>
 	);
 
+	}
 }
 
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
 
 return (
 	<div className="pa2">
 	<input className="pa3 ba b--green bg-lightest-blue"
 	type="search" 
-	placeholder="peruse the cats" />
+	placeholder="peruse the cats" 
+	onChange={searchChange}
+	/>
      </div>
 	)
 
 }
-
 
 
 
